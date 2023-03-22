96992b88581af93847edd95ffd9e17925adbbb18
tiffanynchau <tiffanynchau@gmail.com>
2023-03-21 23:54:53 -0500

    create 2 experiences tabs

---
+++ src/components/Exptab2.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Exptab2 = () => {
+  return (
+    <section className="exp2">
+      <h4>Trust & Safety Legal Specialist</h4>
+      <p>2017 - 2022</p>
+      <ul>
+        <li>
+          Utilized a variety of admin tools and technologies such as automated
+          fraud detection systems, manual review processes, and customer support
+          systems to monitor online content and mitigate risks that may violate
+          trust and safety policies
+        </li>
+        <li>
+          Worked alongside with Google development team in building supporting
+          tools that allow agents to make litigation decisions directly on
+          Google search engine, which boosted productivity wise by 20% and
+          reduced processing time by 35%
+        </li>
+      </ul>
+    </section>
+  );
+};
